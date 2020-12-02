f53cf2dc4000a3700d499d873505d59a91878569
alumno <plajommj@hotmail.com>
2020-12-02 16:59:51 +0100

    Casi terminando el ejercicio 2

--- resources/views/productos/create.blade.php
+++ resources/views/productos/create.blade.php
@@ -2,6 +2,18 @@
 
 @section('content')
 
-    Añadir producto
+    <form action="/" method="post">
+        <label for="inputNombre">Nombre</label>
+        <input type="text" name="nombre" id="inputNombre">
+
+        <label for="precio">Precio</label>
+        <input type="number" name="precio" id="inputPrecio">
+
+        <label for="inputCategoria">Categor&iacute;a</label>
+        <input type="text" name="categoria" id="inputCategoria">
+
+        <label for="inputImagen">Imagen</label>
+        <input type="text" name="imagen" id="inputImagen">
+    </form>
 
 @stop
