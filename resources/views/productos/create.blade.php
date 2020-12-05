70205544e93a08a10966ade8d27cca1f8e8e70ff
alumno <plajommj@hotmail.com>
2020-12-05 12:13:58 +0100

    Terminada tarea 2

--- resources/views/productos/create.blade.php
+++ resources/views/productos/create.blade.php
@@ -2,18 +2,54 @@
 
 @section('content')
 
-    <form action="/" method="post">
-        <label for="inputNombre">Nombre</label>
-        <input type="text" name="nombre" id="inputNombre">
+<div class="row" style="margin-top:40px">
+    <div class="offset-md-3 col-md-6">
+       <div class="card">
+            <div class="card-header text-center">
+                Crear Producto
+            </div>
+            <div class="card-body" style="padding:30px">
 
-        <label for="precio">Precio</label>
-        <input type="number" name="precio" id="inputPrecio">
+            <form action="{{ url('/productos/create') }}" method="POST">
 
-        <label for="inputCategoria">Categor&iacute;a</label>
-        <input type="text" name="categoria" id="inputCategoria">
+                 @csrf
 
-        <label for="inputImagen">Imagen</label>
-        <input type="text" name="imagen" id="inputImagen">
-    </form>
+                 <div class="form-group">
+                    <label for="inputNombre">Nombre</label>
+                    <input type="text" name="nombre" id="inputNombre" class="form-control">
+                 </div>
+
+                 <div class="form-group">
+                    <label for="inputPrecio">Precio</label>
+                    <input type="number" name="precio" id="inputPrecio">
+                 </div>
+
+                 <div class="form-group">
+                     <label for="inputCategoria">Categor&iacute;a</label>
+                    <input type="text" name="categoria" id="inputCategoria" class="form-control">
+                 </div>
+
+                 <div class="form-group">
+                     <label for="inputImagen">Imagen</label>
+                    <input type="text" name="imagen" id="inputImagen" class="form-control">
+                 </div>
+
+                 <div class="form-group">
+                    <label for="textAreaDescripcion">Descripci&oacute;n</label>
+                    <textarea name="descripcion" id="textAreaDescripcion" class="form-control" rows="3"></textarea>
+                 </div>
+
+                 <div class="form-group text-center">
+                    <button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
+                        Crear Nuevo Producto
+                    </button>
+                 </div>
+
+             </form>
+
+          </div>
+       </div>
+    </div>
+ </div>
 
 @stop
--- resources/views/productos/edit.blade.php
+++ resources/views/productos/edit.blade.php
@@ -2,6 +2,55 @@
 
 @section('content')
 
-    Modificar producto {{ $id }}
+<div class="row" style="margin-top:40px">
+    <div class="offset-md-3 col-md-6">
+       <div class="card">
+            <div class="card-header text-center">
+                Modificar Producto
+            </div>
+            <div class="card-body" style="padding:30px">
+
+             <form action="{{ url('/productos/edit/hola') }}" method="POST">
+                {{ method_field('PUT') }}
+
+                 @csrf
+
+                 <div class="form-group">
+                    <label for="inputNombre">Nombre</label>
+                    <input type="text" name="nombre" id="inputNombre" class="form-control">
+                 </div>
+
+                 <div class="form-group">
+                    <label for="inputPrecio">Precio</label>
+                    <input type="number" name="precio" id="inputPrecio">
+                 </div>
+
+                 <div class="form-group">
+                     <label for="inputCategoria">Categor&iacute;a</label>
+                    <input type="text" name="categoria" id="inputCategoria" class="form-control">
+                 </div>
+
+                 <div class="form-group">
+                     <label for="inputImagen">Imagen</label>
+                    <input type="text" name="imagen" id="inputImagen" class="form-control">
+                 </div>
+
+                 <div class="form-group">
+                    <label for="textAreaDescripcion">Descripci&oacute;n</label>
+                    <textarea name="descripcion" id="textAreaDescripcion" class="form-control" rows="3"></textarea>
+                 </div>
+
+                 <div class="form-group text-center">
+                    <button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
+                        Modificar Producto
+                    </button>
+                 </div>
+
+             </form>
+
+          </div>
+       </div>
+    </div>
+ </div>
 
 @stop
